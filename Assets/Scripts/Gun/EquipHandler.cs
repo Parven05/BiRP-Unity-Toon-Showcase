2d2867b47f95353b961f4377c329bb77dd4c3ec8
musthafa45 <52942581+musthafa45@users.noreply.github.com>
2024-01-21 22:12:22 +0400

    hammer anim and easings

--- Assets/Scripts/Gun/EquipHandler.cs
+++ Assets/Scripts/Gun/EquipHandler.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,21 +9,99 @@ public class EquipHandler : MonoBehaviour
     [SerializeField] private Transform remoteTransform;
     [SerializeField] private Transform hammerTransform;
 
+    [SerializeField] private bool isActivePistol = false;
+    [SerializeField] private bool isActiveRemote = false;
+    [SerializeField] private bool isActiveHammer = false;
+
+    [SerializeField] private Ease pistolEaseMode = Ease.InBack;
+    [SerializeField] private Ease remoteEaseMode = Ease.InBack;
+    [SerializeField] private Ease hammerEaseMode = Ease.InBack;
+
+    private void Awake()
+    {
+        SetActivePistol(isActivePistol);
+        SetActiveRemote(isActiveRemote);
+        SetActiveHammer(isActiveHammer);
+    }
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Alpha1))
         {
-            pistolTransform.gameObject.SetActive(!pistolTransform.gameObject.activeSelf);
+            //pistolTransform.gameObject.SetActive(!pistolTransform.gameObject.activeSelf);
+            isActivePistol = !isActivePistol;
+            if (isActivePistol)
+                SetActivePistol(true);
+            else
+                SetActivePistol(false);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            remoteTransform.gameObject.SetActive(!remoteTransform.gameObject.activeSelf);
+            //remoteTransform.gameObject.SetActive(!remoteTransform.gameObject.activeSelf);
+
+            isActiveRemote = !isActiveRemote;
+            if (isActiveRemote)
+                SetActiveRemote(true);
+            else
+                SetActiveRemote(false);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            hammerTransform.gameObject.SetActive(!hammerTransform.gameObject.activeSelf);
+            isActiveHammer = !isActiveHammer;
+            //hammerTransform.gameObject.SetActive(!hammerTransform.gameObject.activeSelf);
+            if (isActiveHammer)
+                SetActiveHammer(true);
+            else
+                SetActiveHammer(false);
+        }
+    }
+
+    private void SetActiveHammer(bool active)
+    {
+        if(active)
+        {
+            hammerTransform.gameObject.SetActive(active);
+            hammerTransform.DOLocalMoveZ(0.82f, 0.2f).SetEase(hammerEaseMode);
+        } 
+        else
+        {
+            hammerTransform.DOLocalMoveZ(0f, 0.2f).SetEase(hammerEaseMode).OnComplete(() =>
+            {
+                hammerTransform.gameObject.SetActive(active);
+            });
+        }
+    }
+
+    private void SetActiveRemote(bool active)
+    {
+        if(active)
+        {
+            remoteTransform.gameObject.SetActive(active);
+            remoteTransform.DOLocalMoveY(7.489f, 0.2f).SetEase(remoteEaseMode);
+        }
+        else
+        {
+            remoteTransform.DOLocalMoveY(5.5f, 0.2f).SetEase(remoteEaseMode).OnComplete(() =>
+            {
+                remoteTransform.gameObject.SetActive(active);
+            });
+        }
+    }
+
+    private void SetActivePistol(bool active)
+    {
+        if(active)
+        {
+            pistolTransform.gameObject.SetActive(active);
+            pistolTransform.DOLocalMoveY(-0.23f, 0.2f).SetEase(pistolEaseMode);
+        }  
+        else
+        {
+            pistolTransform.DOLocalMoveY(-1, 0.2f).SetEase(pistolEaseMode).OnComplete(() =>
+            {
+                pistolTransform.gameObject.SetActive(active);
+            });
         }
     }
 }
--- Assets/Scripts/Melee/Hammer.cs
+++ Assets/Scripts/Melee/Hammer.cs
@@ -5,36 +5,38 @@ using UnityEngine;
 
 public class Hammer : MonoBehaviour
 {
-    [SerializeField] Vector3 endTargetRotation;
-    [SerializeField] float hammerAttackDelay;
-    [SerializeField] Ease hammerEase;
+    [SerializeField] private Animator animator;
+    //[SerializeField] Vector3 endTargetRotation;
+    //[SerializeField] float hammerAttackDelay;
+    //[SerializeField] Ease hammerEase;
 
-    private Vector3 oldRotation;
-    private bool isAttackPlaying;
+    //private Vector3 oldRotation;
+    //private bool isAttackPlaying;
 
-    private void Awake()
-    {
-        oldRotation = transform.eulerAngles;
-    }
+    //private void Awake()
+    //{
+    //    oldRotation = transform.eulerAngles;
+    //}
     private void DoAttack()
     {
         //Quaternion targeRot = Quaternion.LookRotation(endTargetRotation, transform.forward);
         //transform.DORotateQuaternion(targeRot,hammerAttackDelay).SetEase(hammerEase);
-        if (isAttackPlaying) return;
+        //if (isAttackPlaying) return;
 
-        isAttackPlaying = true;
-        transform.DORotate(endTargetRotation, hammerAttackDelay, RotateMode.Fast).SetEase(hammerEase).OnComplete(() =>
-        {
-            transform.DORotate(oldRotation, 0.1f, RotateMode.Fast).SetEase(hammerEase).OnComplete(() =>
-            {
-                isAttackPlaying = false;
-            });
-        });
+        //isAttackPlaying = true;
+        //transform.DORotate(endTargetRotation, hammerAttackDelay, RotateMode.Fast).SetEase(hammerEase).OnComplete(() =>
+        //{
+        //    transform.DORotate(oldRotation, 0.1f, RotateMode.Fast).SetEase(hammerEase).OnComplete(() =>
+        //    {
+        //        isAttackPlaying = false;
+        //    });
+        //});
+        animator.SetTrigger("HammerStrike_1");
     }
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             DoAttack();
         }
