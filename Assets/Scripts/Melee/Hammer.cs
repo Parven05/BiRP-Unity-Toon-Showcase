a4ccd16b8b21229867c2b78c65209e8122632303
musthafa45 <52942581+musthafa45@users.noreply.github.com>
2024-01-22 21:22:58 +0400

    Hammer Update

--- Assets/Scripts/Melee/Hammer.cs
+++ Assets/Scripts/Melee/Hammer.cs
@@ -1,37 +1,27 @@
 using DG.Tweening;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Hammer : MonoBehaviour
 {
     [SerializeField] private Animator animator;
-    //[SerializeField] Vector3 endTargetRotation;
-    //[SerializeField] float hammerAttackDelay;
-    //[SerializeField] Ease hammerEase;
+    private bool canAttack = true;
+    [SerializeField] private float timeBetweenAttack = 1f;
 
-    //private Vector3 oldRotation;
-    //private bool isAttackPlaying;
-
-    //private void Awake()
-    //{
-    //    oldRotation = transform.eulerAngles;
-    //}
     private void DoAttack()
     {
-        //Quaternion targeRot = Quaternion.LookRotation(endTargetRotation, transform.forward);
-        //transform.DORotateQuaternion(targeRot,hammerAttackDelay).SetEase(hammerEase);
-        //if (isAttackPlaying) return;
+        if (canAttack)
+        {
+            animator.SetTrigger("HammerStrike_1");
+            canAttack = false;
+            StartCoroutine(ResetAttack());
+        }
+    }
 
-        //isAttackPlaying = true;
-        //transform.DORotate(endTargetRotation, hammerAttackDelay, RotateMode.Fast).SetEase(hammerEase).OnComplete(() =>
-        //{
-        //    transform.DORotate(oldRotation, 0.1f, RotateMode.Fast).SetEase(hammerEase).OnComplete(() =>
-        //    {
-        //        isAttackPlaying = false;
-        //    });
-        //});
-        animator.SetTrigger("HammerStrike_1");
+    private IEnumerator ResetAttack()
+    {
+        yield return new WaitForSeconds(timeBetweenAttack);
+        canAttack = true;
     }
 
     private void Update()
